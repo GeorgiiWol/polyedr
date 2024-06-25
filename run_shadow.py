9a8ac1424b18e116112bcb7163344febbc0b90ee
GeorgiiWol <143259051+GeorgiiWol@users.noreply.github.com>
2024-06-25 17:26:04 +0300

    Add files via upload

---
+++ run_shadow.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S python3 -B
+
+from time import time
+from common.tk_drawer import TkDrawer
+from shadow.polyedr import Polyedr
+
+tk = TkDrawer()
+try:
+    for name in ["test_per1", "test_per2",
+                 "test_per3", "test_per4", "test_per5"]:
+        print("=============================================================")
+        print(f"Начало работы с полиэдром '{name}'")
+        start_time = time()
+        p = Polyedr(f"data/{name}.geom")
+        p.draw(tk)
+        print(p.per())
+        delta_time = time() - start_time
+        print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
+        input("Hit 'Return' to continue -> ")
+except (EOFError, KeyboardInterrupt):
+    print("\nStop")
+    tk.close()
---
+++ tests/test_polyedr.py
@@ -0,0 +1,60 @@
+import unittest
+from unittest.mock import patch, mock_open
+from shadow.polyedr import Polyedr
+
+
+class TestPolyedr0(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        fake_file_content = """200.0	45.0	45.0	30.0
+8	4	16
+-0.5	-0.5	0.5
+-0.5	0.5	0.5
+0.5	0.5	0.5
+0.5	-0.5	0.5
+-0.5	-0.5	-0.5
+-0.5	0.5	-0.5
+0.5	0.5	-0.5
+0.5	-0.5	-0.5
+4	5    6    2    1
+4	3    2    6    7
+4	3    7    8    4
+4	1    4    8    5"""
+        fake_file_path = 'data/holey_box.geom'
+        with patch('shadow.polyedr.open'.format(__name__),
+                   new=mock_open(read_data=fake_file_content)) as _file:
+            self.polyedr = Polyedr(fake_file_path)
+            _file.assert_called_once_with(fake_file_path)
+
+    def test_num_vertexes(self):
+        self.assertEqual(len(self.polyedr.vertexes), 8)
+
+    def test_num_facets(self):
+        self.assertEqual(len(self.polyedr.facets), 4)
+
+    def test_num_edges(self):
+        self.assertEqual(len(self.polyedr.edges), 16)
+
+
+class TestPolyedr1(unittest.TestCase):
+
+    def test_per1(self):
+        p = Polyedr("data/test_per1.geom")
+        self.assertEqual(p.per(), 0)
+
+    def test_per2(self):
+        p = Polyedr("data/test_per2.geom")
+        self.assertEqual(p.per(), 0)
+
+    def test_per3(self):
+        p = Polyedr("data/test_per3.geom")
+        self.assertAlmostEqual(p.per(), 4)
+
+    def test_per4(self):
+        p = Polyedr("data/test_per4.geom")
+        self.assertAlmostEqual(p.per(), 2)
+
+    def test_per5(self):
+        p = Polyedr("data/test_per5.geom")
+        self.assertAlmostEqual(p.per(), 7)
